702762bf3e9f3eea472264bd0ed88fa5fe44496b
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-25 00:47:09 +0500

    Create protected array of routes in Router class

--- Core/Router.php
+++ Core/Router.php
@@ -4,6 +4,7 @@ namespace APP\Core;
 
 
 class Router {
+    protected array $routes = [];
     public function get(string $path, callable $callback) {
         // Logic to handle GET requests
     }
