d6dc7461ac8d90e9fc975d2950cc9ac5e24d2931
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-26 23:43:07 +0500

    Configure get routes

--- Core/Router.php
+++ Core/Router.php
@@ -4,8 +4,17 @@ namespace APP\Core;
 
 
 class Router {
-    protected array $routes = [];
+    protected array $routes = [
+        'get' => [],
+        'post' => [],
+        'put' => [],
+        'delete' => [],
+        'patch' => [],
+        'options' => [],
+        'head' => [],
+    ];
     public function get(string $path, callable $callback) {
         // Logic to handle GET requests
+        $this->routes['get'][$path] = $callback;
     }
 }
