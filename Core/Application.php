c761ae7791a9ab8bcca2c0df0c51ded6219c2ea6
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-05-27 21:56:39 +0500

    Create instance of request in Application constructor

--- Core/Application.php
+++ Core/Application.php
@@ -5,9 +5,11 @@ namespace APP\Core;
 class Application {
 
     public Router $router;
+    public Request $request;
 
     public function __construct() {
         $this->router = new Router();
+        $this->request = new Request();
     }
 
 
