5c984d646998366466da6df18a9e56a6d6eef945
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-05 22:23:43 +0500

    Update home route

--- public/index.php
+++ public/index.php
@@ -6,9 +6,7 @@ use APP\Core\Application;
 $app = new Application();
 
 
-$app->router->get('/', function() {
-    return 'Welcome to the home page!';
-});
+$app->router->get('/', 'home');
 
 $app->router->get('/about', function() {
     return 'This is the about page.';
