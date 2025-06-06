4ef55034fda72bdec2fdd42380e6e7727b59c681
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-06 23:37:05 +0500

    Set static ROOT_DIR property in constructor

--- Core/Application.php
+++ Core/Application.php
@@ -4,11 +4,13 @@ namespace APP\Core;
 
 class Application {
 
+    public static sring $ROOT_DIR;
     public Router $router;
     public Request $request;
 
     public function __construct($rootPath) {
         // Set the root path for the application
+        self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->router = new Router($this->request);
     }
