c86b5bcfe862245299c489be5357c9a4c75b1495
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-08 10:52:06 +0500

    Fix order of Response and Router initialization in Application class constructor

--- Core/Application.php
+++ Core/Application.php
@@ -15,8 +15,8 @@ class Application {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request, $this->response);
         $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
 
