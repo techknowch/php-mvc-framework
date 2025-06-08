46deb08dae86abc01b705089c7a221cf81033ad4
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-08 10:50:22 +0500

    Pass Response instance to Router constructor in Application class

--- Core/Application.php
+++ Core/Application.php
@@ -15,7 +15,7 @@ class Application {
         self::$ROOT_DIR = $rootPath;
         self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->router = new Router($this->request, $this->response);
         $this->response = new Response();
     }
 
