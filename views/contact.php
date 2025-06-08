d836597360d572a0d6c5afd68d0e9386e9530be6
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-08 13:15:33 +0500

    Refactor contact form layout and structure in contact.php

--- views/contact.php
+++ views/contact.php
@@ -1,28 +1,27 @@
-    <div class="container my-5">
-      <h1>Contact Page</h1>
-      <div class="col-lg-8 px-0">
-        <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
-        <p>Feel free to download or copy-and-paste any parts of this example.</p>
-
-        <hr class="col-1 my-4">
-
-        <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
-        <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
-      </div>
+<div class="container my-5">
+   <h1>Contact Page</h1>
+   <div class="col-lg-8 px-0">
+      <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
+      <p>Feel free to download or copy-and-paste any parts of this example.</p>
+      <hr class="col-1 my-4">
       <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
+         <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+         </div>
+         <div class="form-group">
+            <label for="subject">Subject</label>
+            <input type="text" name="subject" class="form-control" id="subject" placeholder="Subject">
+         </div>
+         <div class="form-group mb-3">
+            <label for="body">Body</label>
+            <textarea type="text" name="body" class="form-control" id="body" placeholder="Message"></textarea>
+         </div>
+         <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <hr class="col-1 my-4">
+      <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
+      <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
+   </div>
+</div>
