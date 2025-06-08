d1e6090a518583d7b17e8d8396e6d663251a318b
Syed Muhammad Ali Naqvi <techknowasia@proton.me>
2025-06-08 13:09:28 +0500

    Add contact form to contact page

--- views/contact.php
+++ views/contact.php
@@ -9,4 +9,20 @@
         <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
         <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
       </div>
+      <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </div>
